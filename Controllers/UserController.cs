a1fc7c4b20ac049d59647a20283688e6ae59c9e1
Brahm Dev Pandey <brahm.pandey@loji.us>
2025-07-26 01:59:56 +0530

    created first model and controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCRUD_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("health")]
+        public string Hello()
+        {
+            return "Hi there! Server is up and running!";
+        }
+    }
+}
